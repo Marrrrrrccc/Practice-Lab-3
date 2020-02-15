aa08fd9110bbf286af07c22035472cb6d9c12512
Marc Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2020-02-15 09:01:43 +0800

    add verification

--- event.js
+++ event.js
@@ -1,3 +1,10 @@
 function sent(){
-    alert("The information has been sent to our database");
+    
+    if(document.getElementById("1").value == "" && document.getElementById("2").value == ""
+    && document.getElementById("3").value == "" && document.getElementById("4").value == ""){
+        alert("Please complete the information needed");
+    }else{
+        alert("The information has been sent to our database");
+    }
+    
 }
