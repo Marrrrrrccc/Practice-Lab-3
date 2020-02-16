015f950c460e0f31b72244e7a5aa58aa1b618df9
Marc Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2020-02-16 15:19:17 +0800

    additional validation

---
+++ js/event.js
@@ -0,0 +1,22 @@
+function sent(){
+    var title = document.getElementById("1").value;
+    var sDate = document.getElementById("2").value;
+    var pDate = document.getElementById("3").value;
+    var eDate = document.getElementById("4").value;
+    
+    if( title == "" &&  sDate== ""
+    &&  pDate== "" &&  eDate ==""){
+        alert("Please complete the information needed");
+    }else if (sDate>eDate){
+        alert("The starting date should come first before the end date");
+    }
+    else if(pDate<sDate){
+        alert("Posting date should come ahead of starting date");
+    }
+    else{
+        alert("The information has been sent to our database");
+    }
+    
+    
+    
+}
