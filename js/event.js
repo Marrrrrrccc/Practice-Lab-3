ce6fa6e463e8e12c7d7d0930129b49353ec49670
Marc Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2020-02-16 16:08:38 +0800

    And to Or

--- js/event.js
+++ js/event.js
@@ -3,9 +3,10 @@ function sent(){
     var sDate = document.getElementById("2").value;
     var pDate = document.getElementById("3").value;
     var eDate = document.getElementById("4").value;
+    var select = document.getElementById("select").value;
     
-    if( title == "" &&  sDate== ""
-    &&  pDate== "" &&  eDate ==""){
+    if( title == "" ||  sDate== ""
+    ||  pDate== "" ||  eDate =="" || select == "Choose One:"){
         alert("Please complete the information needed");
     }else if (sDate>eDate){
         alert("The starting date should come first before the end date");
